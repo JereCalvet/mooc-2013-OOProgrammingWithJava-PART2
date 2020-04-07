bb52a33b5cdf07234dbe1bbfcae9a8821a1cf5ef
Jereaaa <jere_calvet@hotmail.com>
2020-04-06 22:35:56 -0300

    mate lavado de agustin prueba 1

--- week11-week11_34.FileManager/src/FileManager.java
+++ week11-week11_34.FileManager/src/FileManager.java
@@ -10,12 +10,36 @@ import java.util.Scanner;
 public class FileManager {
 
     public List<String> read(String file) throws FileNotFoundException {
-        return null;
+        File fileObj = new File(file);
+        Scanner reader = null;
+        List<String> list = new ArrayList<String>();
+
+        try {
+            reader = new Scanner(fileObj, "UTF-8");
+        } catch (Exception e) {
+            System.out.println("Error reading: " + e.getMessage());
+            return null;
+        }
+
+        while (reader.hasNextLine()) {
+            String next = reader.nextLine();
+            list.add(next);
+        }
+
+        return list;
     }
 
     public void save(String file, String text) throws IOException {
+        FileWriter fileObj = new FileWriter(file, true);
+        fileObj.write(text);
+        fileObj.close();
     }
 
     public void save(String file, List<String> texts) throws IOException {
+        FileWriter fileObj = new FileWriter(file, true);
+        for (String line : texts) {
+            fileObj.write(line + "\n");
+        }
+        fileObj.close();
     }
 }
--- week11-week11_34.FileManager/src/Main.java
+++ week11-week11_34.FileManager/src/Main.java
@@ -1,12 +1,15 @@
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         FileManager t = new FileManager();
 
-        for (String line : t.read("src/testinput1.txt")) {
+        for (String line : t.read("src/testinput2.txt")) {
             System.out.println(line);
         }
+        }
     }
-}
