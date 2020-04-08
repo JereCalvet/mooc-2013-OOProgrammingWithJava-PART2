9076d3f4dc5de9815461ee199bd458f9dc49f282
Jereaaa <jere_calvet@hotmail.com>
2020-04-08 20:20:22 -0300

    :+1:

--- week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
+++ week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
@@ -1,9 +1,10 @@
 package dictionary;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  *
@@ -12,10 +13,22 @@ import java.util.Map;
 public class MindfulDictionary {
 
     private Map<String, String> dictionary;
+    private File file;
 
     public MindfulDictionary() {
         this.dictionary = new HashMap<String, String>();
     }
+    
+    /*
+    respuesta propuesta:
+     utilizo para no repetir en el segundo constructor. siempre me olvido!
+    
+    this();
+    */
+    public MindfulDictionary(String file) {
+        this.dictionary = new HashMap<String, String>();
+        this.file = new File(file);
+    }    
 
     public void add(String word, String translation) {
         if (!dictionary.containsKey(word)) {
@@ -54,6 +67,35 @@ public class MindfulDictionary {
         }
     }
 
+    public boolean load(){
+        try {
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()){
+                String line = fileReader.nextLine();
+                String[] parts = line.split(":");
+                add(parts[0], parts[1]);
+            }
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean save(){
+        try {
+            FileWriter writer = new FileWriter(this.file);
+            for (String key : dictionary.keySet()) {
+                writer.write(key + ":" + dictionary.get(key) + "\n");
+            }
+            writer.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return false;
+    }
+    
     @Override
     public String toString() {
         String data = "";
