b4822b17dcbe138426f3f6f13ec5d406f6682c39
Jereaaa <jere_calvet@hotmail.com>
2020-04-15 22:53:31 -0300

    pintando GUI :S

--- week11-week11_41.DrawingBoard/src/drawing/ui/DrawingBoard.java
+++ week11-week11_41.DrawingBoard/src/drawing/ui/DrawingBoard.java
@@ -13,6 +13,10 @@ public class DrawingBoard extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
-
+        graphics.fillRect(80, 40, 50, 50);
+        graphics.fillRect(250, 40, 50, 50);
+        graphics.fillRect(30, 200, 50, 50);
+        graphics.fillRect(300, 200, 50, 50);
+        graphics.fillRect(80, 250, 220, 50); 
     }
 }
