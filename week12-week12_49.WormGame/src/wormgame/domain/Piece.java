7e04ea3c0e6c90254cb7d2bafb57bb13b52da8ce
Jereaaa <jere_calvet@hotmail.com>
2020-05-06 20:18:19 -0300

    juego: avance en el movimiento del gusano

--- week12-week12_49.WormGame/src/wormgame/domain/Piece.java
+++ week12-week12_49.WormGame/src/wormgame/domain/Piece.java
@@ -14,6 +14,14 @@ public class Piece {
         this.y = y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+ 
     public int getX() {
         return x;
     }
--- week12-week12_49.WormGame/src/wormgame/domain/Worm.java
+++ week12-week12_49.WormGame/src/wormgame/domain/Worm.java
@@ -1,10 +1,82 @@
 package wormgame.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import wormgame.*;
+
 /**
  *
  * @author Jere
  */
-public class Worm {
-    
-    
+public class Worm extends Piece {
+
+    private List<Piece> body;
+    private Direction originalDirection;
+    private boolean canGrow;
+
+    public Worm(int originalX, int originalY, Direction originalDirection) {
+        super(originalX, originalY);
+        this.originalDirection = originalDirection;
+        this.body = new ArrayList<Piece>();
+        this.body.add(this);
+    }
+
+    private void handlePieceMovement(Piece piece, Direction dir) {
+        switch (dir) {
+            case UP:
+                piece.setX(piece.getX() - 1);
+                break;
+            case DOWN:
+                piece.setY(piece.getY() - 1);
+                break;
+            case LEFT:
+                piece.setY(piece.getY() + 1);
+                break;
+            case RIGHT:
+                piece.setX(piece.getX() + 1);
+                break;
+            default:
+                System.out.println("Error: invalid direction.");
+                break;
+        }
+    }
+
+    public Direction getDirection() {
+        return originalDirection;
+    }
+
+    public void setDirection(Direction dir) {
+        originalDirection = dir;
+    }
+
+    public int getLength() {
+        return body.size();
+    }
+
+    public List<Piece> getPieces() {
+        return body;
+    }
+
+    public void move() {
+//        if (getLength() < 3 || canGrow) {
+//            grow();
+//        }
+        for (Piece piece : body) {
+            handlePieceMovement(piece, originalDirection);
+        }
+    }
+
+    public void grow() {
+        //agregar una pieza en la posicion ?de alado?
+        body.add(new Piece(0, 0));
+    }
+
+    public boolean runsIntoItself() {
+        for (Piece piece : body) {
+            if (this.runsInto(piece)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
